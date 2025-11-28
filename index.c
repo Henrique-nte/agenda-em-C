d06a608a0fca79dbd61066c86e1b4ca87349d79c
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-11-27 21:14:08 -0300

    Add files via upload

---
+++ index.c
@@ -0,0 +1,13 @@
+// PROJETO AGENDA EM C
+// ESTUDANTES: HENRIQUE S., YAGO,YURI, GABRIEL FLÔR, EMANOEl.
+
+// BIBLIOTECAS UTILIZADAS
+#include <stdio.h>
+#include <stdlib.h>
+
+// FUNÇÃO PRINCIPAL
+int main(void)
+{
+    printf("Hellor World!");
+    return 0;
+}
